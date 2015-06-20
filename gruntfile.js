ee20d26f42507f9357855dfd67af2fb85c6a683c
Mukesh Gupta <mukeshgupta.2006@gmail.com>
2015-06-20 09:19:57 +0530

    Adding grunt build system

---
+++ gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			task: {
+				src: "src/*.js", 
+				dest: 'dist/qrdecode.js',
+			},
+			options: {
+				'separator': grunt.util.linefeed,
+				'banner': '',
+				'footer': '',
+				'stripBanners': false,
+				'process': false,
+				'sourceMap': false,
+				'sourceMapName': undefined,
+				'sourceMapStyle': 'embed'
+			}
+		},
+		uglify: {
+			task: {
+				src: ['dist/qrdecode.js'], 
+				dest: 'dist/qrdecode.min.js'
+			},
+			options: {
+				'mangle': {},
+				'compress': {},
+				'beautify': false,
+				'expression': false,
+				'report': 'min',
+				'sourceMap': false,
+				'sourceMapName': undefined,
+				'sourceMapIn': undefined,
+				'sourceMapIncludeSources': false,
+				'enclose': undefined,
+				'wrap': undefined,
+				'exportAll': false,
+				'preserveComments': undefined,
+				'banner': '',
+				'footer': ''
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', ['concat', 'uglify']);
+};
